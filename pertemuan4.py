5f12fca89e05fec72a8a5d432f847eaab20e460e
devano2068 <devano27921@gmail.com>
2025-10-21 13:58:56 +0700

    def str

--- pertemuan4.py
+++ pertemuan4.py
@@ -3,3 +3,12 @@ class PersegiPanjang:
         self.panjang = panjang
         self.lebar = lebar
 
+    def keliling(self):
+        return 2 * (self.panjang + self.lebar)
+    
+    def luas(self):
+        return self.panjang * self.lebar
+    
+    def __str__(self):
+        return f"Persegi Panjang, Panjang {self.panjang} cm,dan lebar {self.lebar} cm"
+    
