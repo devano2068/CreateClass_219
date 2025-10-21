0c88cf50b9c267a871523a5e4ab648bc1103c8b7
devano2068 <devano27921@gmail.com>
2025-10-21 14:01:19 +0700

    def keliling

--- pertemuan4.py
+++ pertemuan4.py
@@ -2,3 +2,7 @@ class PersegiPanjang:
     def __init__(self, panjang, lebar):
         self.panjang = panjang
         self.lebar = lebar
+
+    def keliling(self):
+        return 2 * (self.panjang + self.lebar)
+    
