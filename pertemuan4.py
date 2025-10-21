76201a639a45596ccda0a4586c03e734e0fa5196
devano2068 <devano27921@gmail.com>
2025-10-21 14:02:08 +0700

    try

--- pertemuan4.py
+++ pertemuan4.py
@@ -13,3 +13,10 @@ class PersegiPanjang:
         return f"Persegi Panjang, Panjang {self.panjang} cm,dan lebar {self.lebar} cm"
     
 
+try:
+    panjang_input = int(input("Masukkan panjang (cm):"))
+    lebar_input = int(input("Masukkan lebar (cm):"))
+
+    pp = PersegiPanjang(panjang_input, lebar_input)
+    print("Keliling:", pp.keliling(), "cm")
+    print("Luas:", pp.luas(), "cm^2")
